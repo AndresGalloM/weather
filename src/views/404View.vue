d04801997a7c64f661666c760e26e4b4e9084d08
AndresGalloM <felipegallom1011@gmail.com>
2024-01-15 23:02:53 -0500

    View 404 error

---
+++ src/views/404View.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="flex flex-col items-center mt-20">
+    <h1 class="text-9xl font-bold">404</h1>
+    <span class="text-lg text-gray-400 mt-4">Whooops... This page could not be found!</span>
+
+    <RouterLink :to="{name: 'home'}" class="border border-current border-solid rounded-lg mt-8 px-4 py-2 opacity-75 font-bold hover:opacity-90">
+      Take me home
+    </RouterLink>
+  </section>
+</template>
