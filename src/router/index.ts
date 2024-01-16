13db178fa74107f2d731f74a5ed3efc54d24df7c
AndresGalloM <felipegallom1011@gmail.com>
2024-01-15 23:05:02 -0500

    Add vue router

---
+++ src/router/index.ts
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import View404 from '@/views/404View.vue'
+
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: View404
+  }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+  }
+)
+
+export default router
