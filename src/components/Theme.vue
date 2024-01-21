4b296618ff334dbbafa26869a239d2a723afa9b9
AndresGalloM <felipegallom1011@gmail.com>
2024-01-20 22:12:42 -0500

    Button to change the theme

---
+++ src/components/Theme.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { PhMoon, PhSun } from '@phosphor-icons/vue'
+import { useDark, useToggle } from '@vueuse/core'
+import Tooltip from '@/components/Tooltip.vue'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <Tooltip msg="Theme">
+    <button @click="toggleDark()">
+      <component :is="isDark ? PhMoon : PhSun" size="26" weight="bold"/>
+    </button>
+  </Tooltip>
+</template>
