77506a8d8f8bdaac35e48521f7d83100d73e8ca1
AndresGalloM <felipegallom1011@gmail.com>
2024-01-20 22:38:34 -0500

    Link to repo Github

---
+++ src/components/Github.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { PhGithubLogo } from '@phosphor-icons/vue'
+import Tooltip from '@/components/Tooltip.vue'
+</script>
+
+<template>
+  <Tooltip msg="Github">
+    <button>
+      <a href="https://github.com/AndresGalloM/weather" target="_blank">
+        <PhGithubLogo size="26" weight="bold"/>
+      </a>
+    </button>
+  </Tooltip>
+</template>
