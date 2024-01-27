f9ceef5cd2cae4d6d93d4feef9782aaafedce69d
AndresGalloM <felipegallom1011@gmail.com>
2024-01-27 15:00:00 -0500

    Changed route name  view 404

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -3,7 +3,7 @@
     <h1 class="text-9xl font-bold">404</h1>
     <span class="text-lg text-gray-400 mt-4">Whooops... This page could not be found!</span>
 
-    <RouterLink :to="{name: 'home'}" class="border border-current border-solid rounded-lg mt-8 px-4 py-2 opacity-75 font-bold hover:opacity-90">
+    <RouterLink :to="{name: 'location'}" class="border border-current border-solid rounded-lg mt-8 px-4 py-2 opacity-75 font-bold hover:opacity-90">
       Take me home
     </RouterLink>
   </section>
